4f60c8b2ccb0d779bb3857221ea93ac0eb0405d1
leandrowanderley <devleandromcz@gmail.com>
2025-01-30 17:47:56 -0300

    Checkpoint 7

--- MyPlayground.playground/Pages/Checkpoint 7.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Checkpoint 7.xcplaygroundpage/Contents.swift
@@ -1,7 +1,67 @@
-//: [Previous](@previous)
+class Animal {
+    let legs: Int = 4
+    
+    func speak() {
+        print("Animal sound")
+    }
+}
 
-import Foundation
+class Dog: Animal {
+    override func speak() {
+        print("Woof!")
+    }
+}
 
-var greeting = "Hello, playground"
+class Cat: Animal {
+    
+    var isTame: Bool
+        
+    init (isTame: Bool) {
+        self.isTame = isTame
+    }
+    
+    override func speak() {
+        print("Meow!")
+    }
+}
 
-//: [Next](@next)
+class Corgi: Dog {
+    override func speak() {
+        print("Corgi Woof!")
+    }
+}
+
+class Poodle: Dog {
+    override func speak() {
+        print("Poodle Woof!")
+    }
+}
+
+class Persian: Cat {
+    
+    override init(isTame: Bool) {
+        super.init(isTame: isTame)
+    }
+    
+    override func speak() {
+        print("Persian Meow!")
+    }
+}
+
+class Lion: Cat {
+    
+    override init (isTame: Bool){
+        super.init(isTame: isTame)
+    }
+    
+    override func speak() {
+        print("Lion Meow")
+    }
+}
+
+var x = Lion(isTame: false)
+var y = Persian(isTame: true)
+x.speak()
+y.speak()
+
+print(x.legs)
